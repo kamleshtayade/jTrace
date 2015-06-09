ead48d514dff437a85426a59c093ea066a17f40a
Kamlesh <kamleshtayade@gmail.com>
2015-06-09 18:43:00 +0530

    bug fixes - specification and bomheader, name change

--- src/main/webapp/scripts/app/entities/itemmtr/itemmtr.controller.js
+++ src/main/webapp/scripts/app/entities/itemmtr/itemmtr.controller.js
@@ -6,6 +6,63 @@ angular.module('jtraceApp')
         $scope.itemsubcats = Itemsubcat.query();
         $scope.itemcats = Itemcat.query();
 
+		$scope.newSubCat1 =[];
+		$scope.subCatValue = {};
+		/* Start : populate the dropdown values for add more specification*/ 
+        $scope.loadSpec = function() {
+            Itemsubcat.query(function(result, headers) {
+            var itemmtrs1 = [];
+            var newSubCat = [];
+            var newSubCat2 = {};
+            var newSub = {};
+
+            $scope.p = "";
+            $scope.p = $scope.itemmtr.itemsubcat.id;
+            itemmtrs1 = result;
+                for(var i = 0 ; i<itemmtrs1.length; i++){
+                    if(itemmtrs1[i].id == $scope.p ){
+                        itemmtrs1[0] = {
+                            spec1 : [{name: 'Tollerance'}, {name:'value'}, {name:'FilmRegister'}]
+                        };
+                        itemmtrs1[1] = {
+                            spec1 : [{name: 'Voltage'}, {name:'Value'}, {name:'Wattage'}]
+                        };
+                        itemmtrs1[2] = {
+                            spec1 : [{name: 'Voltage'}, {name:'Value'}, {name:'CE'}]
+                        };
+                        itemmtrs1[3] = {
+                            spec1 : [{name: 'Value'}, {name:'wattage'}, {name:'Voltage'}]
+                        };
+                        itemmtrs1[4] = {
+                            spec1 : [{name: 'wire'}, {name:'color'}, {name:'gage'}]
+                        };
+
+                        $scope.newSubCat1 = itemmtrs1[i].spec1;
+                    }
+                }
+            });
+        };
+
+        /* End*/
+        /* Start : populate the data in specification textfield */ 
+        $scope.showSpecification = function() {
+            $scope.dropValue2 = "";
+            $scope.itemmtr.specification = ""; 
+
+            for(var i = 0 ;i<$scope.choices.length ; i++){
+                $scope.dropValue = $scope.choices[i].itemsubcat.name.name;
+                $scope.dropValue1 = $scope.choices[i].name;
+
+                //alert($scope.dropValue);
+                if(($scope.dropValue1 != undefined) && (i>0)){
+                    $scope.itemmtr.specification = $scope.itemmtr.specification + ',' + '' + $scope.dropValue1 + '' ;
+                }else if($scope.dropValue1 != undefined && (i == 0)){
+                    $scope.itemmtr.specification = $scope.itemmtr.specification + $scope.dropValue1 ;
+                }
+            }
+        };
+        /* End*/ 
+
         $scope.loadAll = function() {
             Itemmtr.query(function(result, headers) {
                 $scope.itemmtrs = result;
@@ -15,6 +72,7 @@ angular.module('jtraceApp')
         $scope.loadAll();
 
         $scope.create = function () {
+			$scope.removeChoice();
             Itemmtr.update($scope.itemmtr,
                 function () {
                     $scope.loadAll();
@@ -24,7 +82,44 @@ angular.module('jtraceApp')
         };
 
         $scope.update = function (id) {
+			$scope.removeChoice();
             Itemmtr.get({id: id}, function(result) {
+				Itemsubcat.query(function(result, headers) {
+            var itemmtrs1 = [];
+            $scope.p = "";
+            $scope.p = $scope.itemmtr.itemsubcat.id 
+
+            itemmtrs1 = result;
+            var newSubCat = [];
+
+
+            var newSubCat2 = {};
+            var newSub = {};
+
+
+            for(var i = 0 ; i<itemmtrs1.length; i++){
+                if(itemmtrs1[i].id == $scope.p ){
+                    itemmtrs1[0] = {
+                        spec1 : [{name: 'Tollerance'}, {name:'value'}, {name:'FilmRegister'}]
+                    };
+                    itemmtrs1[1] = {
+                        spec1 : [{name: 'Voltage'}, {name:'Value'}, {name:'Wattage'}]
+                    };
+                    itemmtrs1[2] = {
+                        spec1 : [{name: 'Voltage'}, {name:'Value'}, {name:'CE'}]
+                    };
+                    itemmtrs1[3] = {
+                        spec1 : [{name: 'Value'}, {name:'Wattage'}, {name:'Voltage'}]
+                    };
+                    itemmtrs1[4] = {
+                        spec1 : [{name: 'wire'}, {name:'color'}, {name:'gage'}]
+                    };
+
+                    $scope.newSubCat1 = itemmtrs1[i].spec1;
+                    }
+                }
+            });
+
                 $scope.itemmtr = result;
                 $('#saveItemmtrModal').modal('show');
             });
@@ -51,6 +146,9 @@ angular.module('jtraceApp')
             $scope.editForm.$setPristine();
             $scope.editForm.$setUntouched();
         };
+		$scope.clearData = function () {
+			$scope.itemmtr.specification = "";
+		};
 
         /** Start: SetCode
             Item Master Code is required 
